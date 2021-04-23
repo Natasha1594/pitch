6e4fa5141e3970fa03dd742a2c5315eb520f83af
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-04-23 14:12:31 +0300

    updated errors.py

--- app/main/errors.py
+++ app/main/errors.py
@@ -5,7 +5,7 @@ from . import main
 @main.app_errorhandler(404)
 def four_Ow_four(error):
     '''
-    Function to render the 404 error page
+    This is the function that renders the 404 error page
     '''
     title = '404 page'
     return render_template('fourOwfour.html', title=title),404
